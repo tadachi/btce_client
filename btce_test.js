924b33ab6b94c53fde0e22a35a799388ad819d6b
Takumi Adachi <takumiadachi@gmail.com>
2014-05-01 07:59:43 -0700

    Update btce_test.js

--- btce_test.js
+++ btce_test.js
@@ -4,15 +4,13 @@
  */
 
 var btce    = require("./btce.js");
-var logger = require('./logger.js');
+//var logger = require('./logger.js');
 
-//var btce_api_key = 'PKJ6PJQA-ELBJ9IHT-3P1197HP-9GGKJ7LV-72U5DFVA'   
-//var btce_api_secret = '0cfe0b4fd53a62d077ec5cf5322cc3c74410fbd0b30aea3c7f8af7cda0cbf303'
-var btce_api_key = '4GZYCKYA-YKZB2R02-ME3C1UXX-90GQHE3W-I0ZLUS29';
-var btce_api_secret = '54945039c2428ca7a118bb6ed3003cf32abec2af2025eb5d07a291bd3392f411';
+var btce_api_key = '';
+var btce_api_secret = ''
 
 var btceUser = new btce();
-var logger1 = new logger('./btce_test.log');
+var logger1 = ''
 btceUser.logger = logger1;
 btceUser.logger.start();
 
